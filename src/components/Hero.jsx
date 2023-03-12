a89469357f43e1405b857e8220479461999bc478
MuhammadAli511 <mohammadali417@outlook.com>
2023-03-12 19:25:26 +0500

    Navbar Complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import React from 'react';
+import { styles } from "../styles";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className='sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex'>
+
+        
+      </div>
+    </section>
+
   )
 }
 
