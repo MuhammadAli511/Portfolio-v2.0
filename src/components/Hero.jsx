f35c10b77c4dc68d64d7977c9680b9244fa2ef59
MuhammadAli511 <mohammadali417@outlook.com>
2023-03-13 21:22:52 +0500

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,22 @@ import { styles } from "../styles";
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className='sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex'>
-
-        
+      <div className='pt-40 text-center '>
+          <div className="">
+            <span className='roboto-font2 text-[2rem] md:text-[4rem] lg:text-[6rem]'>Hi! I'm <span className='text-font-purple roboto-font3'>Muhammad Ali</span></span>
+            <br className='first-br'></br>
+            <span className='roboto-font2 text-[2rem] md:text-[4rem] lg:text-[6rem]'>A Software Engineer.</span>
+            <br></br>
+            <br></br>
+            <span className='roboto-font text-[0.82rem] md:text-[1rem] lg:text-[1.2rem]'>I also specialize in creating innovative and user-centric web/app</span>
+            <br></br>
+            <span className='roboto-font text-[0.82rem] md:text-[1rem] lg:text-[1.2rem]'>experiences that connect people and ideas in meaningful ways</span>
+          </div>
       </div>
     </section>
 
   )
 }
+
 
 export default Hero
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,12 +6,14 @@ module.exports = {
     extend: {
       colors: {
         primary: "#050816",
+        primary2: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
-        "blue1": "#00b0ff"
+        "blue1": "#00b0ff",
+        "font-purple": '#9552ff'
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
@@ -20,7 +22,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/gradient.png')",
       },
     },
   },
